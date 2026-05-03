79c28421a5d7db3f3de4611ff7a1390a2f825188
lunawj <wesleyluna1@gmail.com>
2026-05-03 18:39:19 -0400

    code clean up

--- main.cpp
+++ main.cpp
@@ -7,9 +7,6 @@ using namespace std;
 
 int main()
 {
-   //int adc_fd = wiringPiI2CSetup(0x48);
-   //int result = wiringPiI2CWriteReg8(fd, 0x40, (i & 0xfff) );
-   
    std::thread run_thread(run); 
    run_thread.join();
    cout << "Exit Condition met. Exiting.." << endl;
